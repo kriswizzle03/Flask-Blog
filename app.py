d788fd7e7521b54fb25478f9f5ef6ddbc8876304
kriswizzle03 <krislmor21@gmail.com>
2024-11-01 15:32:46 -0500

    Added code to display title and content of specified post for editing

--- app.py
+++ app.py
@@ -19,6 +19,18 @@ def get_db_connection():
     #return the connection object
     return conn
 
+#function to retrieve a post from the database
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
+    conn.close()
+
+    if post is None:
+        abort(404)
+
+    return post
+
+
 
 # use the app.route() decorator to create a Flask view function called index()
 @app.route('/')
@@ -61,6 +73,22 @@ def create():
 
     
     return render_template('create.html')
+
+#create a route to edit a post. Load page with get or post method
+#pass the post id as url parameter
+@app.route('/<int:id>/edit', methods=('GET','POST'))
+def edit(id):
+    #get the post from the database with a select query for the post with that id
+    post = get_post(id)
+
+    #determine if the page was requested with GET or POST
+    if request.method == 'POST':
+        pass
+    
+        #if POST, process the form data. Get the data and validate it. Update the post and redirect to the homepage.
+
+    #if GET then display page
+    return render_template('edit.html', post=post)
 
 
 app.run(port=5008)
