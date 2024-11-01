00cb64ad6924bc1392297c13a151e875bddb0a25
kriswizzle03 <krislmor21@gmail.com>
2024-11-01 10:23:26 -0500

    Added functionality for creating new blog posts. Implemented error checking for blank title and/or content of new posts

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, url_for, flash, redirect, abo
 # make a Flask application object called app
 app = Flask(__name__)
 app.config["DEBUG"] = True
-
+app.config['SECRET_KEY'] = 'your secret key'
 
 
 # Function to open a connection to the database.db file
@@ -41,7 +41,24 @@ def index():
 def create():
     #determine if the page is being requested with a POST or GET request
     if request.method == 'POST':
-        pass
+        #get the title and content that was submitted
+        title = request.form['title']
+        content = request.form['content']
+
+        #display an error if title or content is not submitted
+        #else make a database connection and insert the blog post content
+        if not title:
+            flash("Title is required")
+        elif not content:
+            flash("Content is required")
+        else:
+            conn = get_db_connection()
+            #insert data into database
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)', (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
     
     return render_template('create.html')
 
